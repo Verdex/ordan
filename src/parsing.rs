48555afdfd2b33def6145ddd38b751c1ef4ae27b
David L. Inman <davidlevere@gmail.com>
2025-04-13 18:16:23 -0400

    parse and use clauses

--- src/parsing.rs
+++ src/parsing.rs
@@ -20,17 +20,17 @@ macro_rules! proj {
 }
 
 pub (crate) fn parse(input : &mut Parser<TokenTree>) -> Result<Pattern, ()> {
-    let patterns = {
-        let first_pattern = pattern(input)?;
+    let clauses = {
+        let first_clause = clause(input)?;
 
-        let mut rest_pattern = input.list(|input| {
+        let mut rest_clause = input.list(|input| {
             proj!(input, TokenTree::Punct(p) if p.as_char() == ';', ())?;
-            pattern(input)
+            clause(input)
         })?;
 
-        rest_pattern.insert(0, first_pattern);
+        rest_clause.insert(0, first_clause);
 
-        rest_pattern
+        rest_clause
     };
 
     proj!(input, TokenTree::Punct(p) if p.as_char() == '=', ())?;
@@ -38,9 +38,25 @@ pub (crate) fn parse(input : &mut Parser<TokenTree>) -> Result<Pattern, ()> {
 
     let return_expr : Rc<str> = input.list(|input| Ok(input.get(())?.to_string()))?.join("").into();   // TODO get error
 
-    Ok(Pattern { patterns, return_expr })
+    Ok(Pattern { clauses, return_expr })
 }
 
-fn pattern(input : &mut Parser<TokenTree>) -> Result<Rc<str>, ()> {
-    proj!(input, TokenTree::Group(g) if g.delimiter() == Delimiter::Bracket, g.stream().to_string().into())
+fn clause(input : &mut Parser<TokenTree>) -> Result<Clause, ()> {
+    let pattern = proj!(input, TokenTree::Group(g) if g.delimiter() == Delimiter::Bracket, g.stream().to_string().into())?;
+    match input.option(|input| nexts(input))? {
+        Some(nexts) => Ok(Clause { pattern, nexts }),
+        None => Ok(Clause { pattern, nexts: vec![] }),
+    }
+}
+
+fn nexts(input : &mut Parser<TokenTree>) -> Result<Vec<Rc<str>>, ()> {
+    let first : Rc<str> = proj!(input, x @ TokenTree::Ident(_), x.to_string())?.into();
+    let mut rest : Vec<Rc<str>> = input.list(|input| {
+        proj!(input, TokenTree::Punct(p) if p.as_char() == ',', ())?;
+        proj!(input, x @ TokenTree::Ident(_), x.to_string().into())
+    })?;
+
+    rest.insert(0, first);
+
+    Ok(rest)
 }
