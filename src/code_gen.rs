d9248a139c68e7041c6cf6ec7039c70edc31c198
David L. Inman <davidlevere@gmail.com>
2025-04-15 21:30:13 -0400

    fix gen

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -7,13 +7,21 @@ pub (crate) fn gen_pattern(mut pattern : Pattern) -> Rc<str> {
 
     let mut ret : Rc<str> = pattern.return_expr;
     while let Some(clause) = pattern.clauses.pop() {
+        if clause.nexts.len() == 0 {
+            ret = gen_match("input", &clause.pattern, &ret);
+        }
+        else {
+            ret = clause.nexts.into_iter()
+                .map(|next| gen_match(&next, &clause.pattern, &ret))
+                .collect::<Vec<_>>()
+                .join("\n").into();
+        }
     }
-    //let v = gen_match("input", &pattern.clauses[0].pattern, &pattern.return_expr);
 
     format!("|input| {}", ret).into()
 }
 
-fn gen_match(input : &str, pattern : &str, expr : &str ) -> Rc<str> {
+fn gen_match(input : &str, pattern : &str, expr : &str) -> Rc<str> {
     format!("match {} {{
         {} => {{ {} }}, 
         _ => {{ }},
--- tests/basic.rs
+++ tests/basic.rs
@@ -3,9 +3,9 @@ use ordan::*;
 
 #[test]
 fn should() {
-    let x = (1, 2);
+    let x = ((1, 2), (3, 4));
     let mut o = 0;
-    let mut m = blarg!([ (y, z) ] y, z ; [(w, h)] => o = y + z);
+    let mut m = blarg!([ (y, z) ] y, z ; [(w, h)] => o += w + h);
     m(x);
 
     assert_eq!(o, 3);
