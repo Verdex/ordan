3d3602f3d50b7596ea39c43ffcbd50310232323a
David L. Inman <davidlevere@gmail.com>
2025-04-15 22:01:02 -0400

    code gen inc prog

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -2,23 +2,45 @@
 use std::rc::Rc;
 use crate::data::*;
 
-
 pub (crate) fn gen_pattern(mut pattern : Pattern) -> Rc<str> {
 
-    let mut ret : Rc<str> = pattern.return_expr;
+    let mut ret = R::ReturnExpr(pattern.return_expr);
     while let Some(clause) = pattern.clauses.pop() {
         if clause.nexts.len() == 0 {
-            ret = gen_match("input", &clause.pattern, &ret);
+            ret = R::Match { input: "input".into(), pattern: Rc::clone(&clause.pattern), expr: Rc::new(ret) };
         }
         else {
-            ret = clause.nexts.into_iter()
-                .map(|next| gen_match(&next, &clause.pattern, &ret))
-                .collect::<Vec<_>>()
-                .join("\n").into();
+            ret = R::SyntaxList(clause.nexts.into_iter()
+                .map(|next| R::Match { input: next, pattern: Rc::clone(&clause.pattern), expr: Rc::new(ret.clone()) })
+                .collect::<Vec<_>>())
         }
     }
 
-    format!("|input| {}", ret).into()
+    r_to_str(ret)
+}
+
+enum R {
+    Match { input : Rc<str>, pattern : Rc<str>, expr : Rc<R> },
+    ReturnExpr(Rc<str>),
+    SyntaxList(Vec<R>),
+}
+
+impl Clone for R {
+    fn clone(&self) -> Self {
+        match self {
+            R::Match { input, pattern, expr } => R::Match { 
+                input: Rc::clone(input), 
+                pattern: Rc::clone(pattern), 
+                expr: Rc::clone(expr)
+            },
+            R::ReturnExpr(s) => R::ReturnExpr(Rc::clone(s)),
+            R::SyntaxList(l) => R::SyntaxList(l.clone()),
+        }
+    }
+}
+
+fn r_to_str(input : R) -> Rc<str> {
+    todo!()
 }
 
 fn gen_match(input : &str, pattern : &str, expr : &str) -> Rc<str> {
