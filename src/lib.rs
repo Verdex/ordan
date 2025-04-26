536c31b34012bbb73132411896084e3aa31f926e
David L. Inman <davidlevere@gmail.com>
2025-04-26 13:52:03 -0400

    clean out old notes

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ mod code_gen;
 use proc_macro::*;
 use jlnexus::Parser;
 
-
 #[proc_macro]
 pub fn s_pattern( input : TokenStream ) -> TokenStream {
     let input = input.into_iter().collect::<Vec<_>>();
@@ -14,62 +13,4 @@ pub fn s_pattern( input : TokenStream ) -> TokenStream {
     let result = parsing::parse(&mut parser).unwrap();
 
     code_gen::gen_pattern(result).parse().unwrap()
-    //format!("panic!(\"{{}}\", \"{} :: {}\");", result.patterns.join(","), result.return_expr).parse().unwrap()
-}
-/*
-[ p1[x] ] x ; [ p2[y] ] => { (x, y) }
-
-match w {
-    p1 => {
-        match x {
-            p2 => {
-                yield (x, y)
-            },
-            _ => { },
-        },
-    },
-    _ => {  },
-}
-
-[ p1 ] x, y ; [ p2[w, z] ] => { (w, z) }
-
-match w {
-    p1 => {
-        match x {
-            p2 => {
-                yield (w, z)
-            }
-        },
-        match y {
-            p2 => {
-                yield (w, z)
-            }
-        },
-    },
-}
-*/
-
-#[cfg(test)]
-mod test {
-    struct X(usize);
-
-    #[test]
-    fn test() {
-
-        let x = X(0);
-        let addr_x = &x;
-
-        let mut w = None;
-
-        let i = std::iter::from_fn(move || { 
-            if w.is_none() {
-                w = Some(std::iter::repeat(addr_x).map(|h| h.0));
-            }
-            w.as_mut().unwrap().next()
-        });
-
-        let z = i.take(5).collect::<Vec<_>>();
-
-        //assert_eq!(z, []);
-    }
 }
