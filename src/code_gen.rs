0a418f989f99949bb7156113e0fa625ac2cd5191
David L. Inman <davidlevere@gmail.com>
2025-04-28 21:43:42 -0400

    inc prog for slice

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -10,11 +10,21 @@ pub (crate) fn gen_pattern(mut pattern : Pattern) -> Rc<str> {
 
     while let Some(clause) = pattern.clauses.pop() {
         if clause.nexts.len() == 0 {
-            ret = R::Match { input: format!("input_{TAG}").into(), pattern: Rc::clone(&clause.pattern), expr: Box::new(ret) };
+            ret = R::Match { 
+                input: format!("input_{TAG}").into(), 
+                pattern: Rc::clone(&clause.pattern), 
+                expr: Box::new(ret),
+                slice: clause.slice,
+            };
         }
         else {
             ret = R::SyntaxList(clause.nexts.into_iter()
-                .map(|next| R::Match { input: next, pattern: Rc::clone(&clause.pattern), expr: Box::new(ret.clone()) })
+                .map(|next| R::Match { 
+                    input: next, 
+                    pattern: Rc::clone(&clause.pattern), 
+                    expr: Box::new(ret.clone()),
+                    slice: clause.slice,
+                })
                 .collect::<Vec<_>>())
         }
     }
@@ -47,7 +57,7 @@ pub (crate) fn gen_pattern(mut pattern : Pattern) -> Rc<str> {
 }
 
 enum R {
-    Match { input : Rc<str>, pattern : Rc<str>, expr : Box<R> },
+    Match { input : Rc<str>, pattern : Rc<str>, expr : Box<R>, slice : bool },
     ReturnExpr(Rc<str>),
     SyntaxList(Vec<R>),
 }
@@ -55,10 +65,11 @@ enum R {
 impl Clone for R {
     fn clone(&self) -> Self {
         match self {
-            R::Match { input, pattern, expr } => R::Match { 
+            R::Match { input, pattern, expr, slice } => R::Match { 
                 input: Rc::clone(input), 
                 pattern: Rc::clone(pattern), 
                 expr: expr.clone(),
+                slice: *slice,
             },
             R::ReturnExpr(s) => R::ReturnExpr(Rc::clone(s)),
             R::SyntaxList(l) => R::SyntaxList(l.clone()),
@@ -70,8 +81,14 @@ static mut ID : usize = 0;
 
 fn r_to_str(input : &R) -> Rc<str> {
     match input {
-        R::Match { input, pattern, expr } => {
+        R::Match { input, pattern, expr, slice: false } => {
             format!("match {input}.borrow() {{
+                {pattern} => {{ {} }}, 
+                _ => {{ }},
+            }}", r_to_str(expr)).into()
+        }, 
+        R::Match { input, pattern, expr, slice: true } => {
+            format!("match &{input}[..] {{
                 {pattern} => {{ {} }}, 
                 _ => {{ }},
             }}", r_to_str(expr)).into()
